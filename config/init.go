16f00123cdf207151de91087d9f6564db6bb9be9
Wieneo <johann@gnaucke.com>
2021-09-01 10:34:21 +0200

    Added basic webserver and env configuration

--- config/init.go
+++ config/init.go
@@ -3,17 +3,21 @@ package config
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"os"
 	"runtime"
+	"strconv"
 
 	"gitlab.cloud.spuda.net/Wieneo/golangutils/v2/logger"
 	"gitlab.cloud.spuda.net/Wieneo/golangutils/v2/stringHelper"
 )
 
 type SampleConfig struct {
-	ListenAddress string
-	ListenPort    int
-	CachePath     string
+	ListenAddress      string
+	ListenPort         int
+	CachePath          string
+	ServerAddress      string
+	ServerAdressParsed url.URL
 }
 
 var Config SampleConfig
@@ -30,6 +34,13 @@ const loggingArea = "CONFIG"
 
 //Init initializes the config struct
 func Init() {
+	var useENV bool
+	var err error
+	if !stringHelper.IsEmpty(os.Getenv("Flow_ENV")) {
+		useENV, err = strconv.ParseBool(os.Getenv("Flow_ENV"))
+	}
+
+	//Determine OS and use specific paths for config and cache
 	switch runtime.GOOS {
 	case "windows":
 		configPath = windowsConfigPath
@@ -41,27 +52,74 @@ func Init() {
 		logger.Fatal(loggingArea, "Agent is running on unsupported platform:", runtime.GOOS)
 	}
 
-	content, err := os.ReadFile(configPath)
-	if os.IsNotExist(err) {
-		logger.Fatal(loggingArea, fmt.Sprintf("No such file or directory: %s. Was the agent intalled properly?", configPath))
-	}
+	if !useENV || err != nil {
+		content, err := os.ReadFile(configPath)
+		if os.IsNotExist(err) {
+			logger.Fatal(loggingArea, fmt.Sprintf("No such file or directory: %s. Was the agent intalled properly?", configPath))
+		}
 
-	if err := json.Unmarshal(content, &Config); err != nil {
-		logger.Fatal(loggingArea, "Couldn't parse config:", err)
+		if err := json.Unmarshal(content, &Config); err != nil {
+			logger.Fatal(loggingArea, "Couldn't parse config:", err)
+		}
+	} else {
+		readEnv()
 	}
 
 	//Sanity check config
 	if stringHelper.IsEmpty(Config.ListenAddress) {
-		logger.Fatal(loggingArea, "ListenAddress is malformed. Example: \"0.0.0.0\"")
+		if !useENV {
+			logger.Fatal(loggingArea, "ListenAddress is malformed. Example: \"0.0.0.0\"")
+		} else {
+			logger.Warning(loggingArea, "Using default ListenAddress: 0.0.0.0")
+			Config.ListenAddress = "0.0.0.0"
+		}
 	}
 
 	if Config.ListenPort == 0 {
-		logger.Fatal(loggingArea, "ListenPort is malformed. Example: 5000")
+		if !useENV {
+			logger.Fatal(loggingArea, "ListenPort is malformed. Example: 5000")
+		} else {
+			logger.Warning(loggingArea, "Using default ListenPort: 5000")
+			Config.ListenPort = 5000
+		}
 	}
 
 	if stringHelper.IsEmpty(Config.CachePath) {
-		logger.Fatal(loggingArea, "CachePath is malformed. Example:", cacheExamplePath)
+		if !useENV {
+			logger.Fatal(loggingArea, "CachePath is malformed. Example:", cacheExamplePath)
+		} else {
+			logger.Warning(loggingArea, "Using default CachePath:", cacheExamplePath)
+			Config.CachePath = cacheExamplePath
+		}
 	}
 
+	if stringHelper.IsEmpty(Config.ServerAddress) {
+		if !useENV {
+			logger.Fatal(loggingArea, "ServerAdress is malformed. Example: http://my-server.domain.tld:5000")
+		} else {
+			logger.Fatal(loggingArea, "ServerAddress is malformed / not set. Please set the ENV Variable: Flow_ServerAddress")
+		}
+	}
+
+	//Parse ServerAddress for easier use later
+	url, err := url.Parse(Config.ServerAddress)
+	if err != nil {
+		logger.Fatal(loggingArea, "Couldn't parse ServerAddress:", err)
+	}
+
+	Config.ServerAdressParsed = *url
+
 	logger.Info(loggingArea, "Config is operational")
+}
+
+func readEnv() {
+	logger.Info(loggingArea, "Using ENV Variables as config")
+
+	Config.ListenAddress = os.Getenv("Flow_ListenAddress")
+	Config.CachePath = os.Getenv("Flow_CachePath")
+	Config.ServerAddress = os.Getenv("Flow_ServerAddress")
+
+	if port, err := strconv.Atoi(os.Getenv("Flow_ListenPort")); err == nil {
+		Config.ListenPort = port
+	}
 }
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"gitlab.cloud.spuda.net/Wieneo/golangutils/v2/logger"
 	"gitlab.cloud.spuda.net/flowkeeper/flowagent/v2/cache"
 	"gitlab.cloud.spuda.net/flowkeeper/flowagent/v2/config"
+	"gitlab.cloud.spuda.net/flowkeeper/flowagent/v2/webserver"
 )
 
 func main() {
 	logger.Info("MAIN", "Starting FlowKeeper FlowAgent")
 	config.Init()
 	cache.Init()
+	webserver.Init()
 }
